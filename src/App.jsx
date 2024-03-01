bc2688215ab4f3c68bcd15ea8fb0d359bab1faff
Ragidimilli Anuragh <Princeanuragh1@gmail.com>
2024-03-01 13:59:48 -0600

    updtd

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,29 @@
+import styled from 'styled-components';
+import GlobalStyles from './styles/Globalstyles';
+import Button from './ui/Button';
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: green;
+`;
+
+const StyledApp = styled.div`
+  background-color: lightgreen;
+  padding: 20px;
+`;
+
 function App() {
-  return <div>Hello World</div>
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>sillyParadise</H1>
+        <Button onClick={() => alert('check in')}>Check In</Button>
+        <Button onClick={() => alert('check out')}>Check Out</Button>
+      </StyledApp>
+    </>
+  );
 }
 
-export default App
+export default App;
