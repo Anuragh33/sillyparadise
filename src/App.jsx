b0cc1c079464268e3b6c57abac21a27938ec328c
Ragidimilli Anuragh <Princeanuragh1@gmail.com>
2024-03-01 20:25:36 -0600

    updtd

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
 import GlobalStyles from './styles/Globalstyles';
 import Button from './ui/Button';
+import Heading from './ui/Heading';
+import Input from './ui/Input';
+import Row from './ui/Row';
 
 const H1 = styled.h1`
   font-size: 30px;
@@ -9,7 +12,7 @@ const H1 = styled.h1`
 `;
 
 const StyledApp = styled.div`
-  background-color: lightgreen;
+  //background-color: lightgreen;
   padding: 20px;
 `;
 
@@ -18,9 +21,25 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <H1>sillyParadise</H1>
-        <Button onClick={() => alert('check in')}>Check In</Button>
-        <Button onClick={() => alert('check out')}>Check Out</Button>
+        <Row>
+          <Heading as='h1'>sillyParadise</Heading>
+
+          <div>
+            <Heading as='h2'>Check In and Out</Heading>
+
+            <Button onClick={() => alert('check in')}>Check In</Button>
+            <Button onClick={() => alert('check out')}>Check Out</Button>
+          </div>
+        </Row>
+
+        <Row>
+          <Heading as='h3'>sillyParadise</Heading>
+
+          <div>
+            <Input type='number' placeholder='Number of Guests' />
+            <Input type='number' placeholder='Number of Guests' />
+          </div>
+        </Row>
       </StyledApp>
     </>
   );
