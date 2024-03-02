bf64eddbb63f6ff47172fd281cbbb14961ec3f20
Ragidimilli Anuragh <Princeanuragh1@gmail.com>
2024-03-02 02:33:00 -0600

    updtd

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://iiimsgqnuumjbybcremo.supabase.co';
+const supabaseKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlpaW1zZ3FudXVtamJ5YmNyZW1vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDkzNjAxMDcsImV4cCI6MjAyNDkzNjEwN30.3HABtplEt7HxfPSwGNClqt402EFX5NEzijBfnPt0pbw';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
